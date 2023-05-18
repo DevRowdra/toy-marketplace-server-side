cf6ade44e20e15d22637a13f0f3171129e046665
DevRowdra <121636096+DevRowdra@users.noreply.github.com>
2023-05-18 19:44:51 +0600

    instoll donenv and make user and password safe

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const app=express()
+require('dotenv').config()
 const port=process.env.PORT || 3000;
+
 app.use(cors())
 app.use(express.json())
 
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://toyMarketPlace:ENvwrS7a0ObxCWLC@cluster0.geakxzz.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.geakxzz.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
